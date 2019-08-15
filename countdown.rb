16c5f2714c4223b9e0899a686bc80395b40913bb
James Nash <nash.jamesm@gmail.com>
2019-08-15 15:44:55 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,8 @@
 #write your code here
 
 def countdown
-  x = 10
-while x > 0
-  puts "#{x} is less than 10"
-  x += 1
+  number = 10
+while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
 end
