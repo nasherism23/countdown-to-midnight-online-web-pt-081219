41664d8b1fb48d477fc2d4f7407b8e8b399a16a0
James Nash <nash.jamesm@gmail.com>
2019-08-15 15:49:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,18 @@
 #write your code here
 
-def countdown
-   number = 10
-  while number > 0
-    puts "#{number} SECOND(S)!"
-    number -= 1
+def countdown(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(seconds)
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    sleep(1)
+    seconds -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
